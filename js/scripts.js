53c0717fbf5dc25df41537e57ed242b452a66c24
Nikki Boyd and Renee Sarley <renee@>
2018-06-11 16:19:50 -0700

    add more javascript to append and remove items to list

--- js/scripts.js
+++ js/scripts.js
@@ -7,35 +7,57 @@ function Task(task, duedate, urgency, category, notes) {
   this.notesInfo = notes;
 }
 
+  function showTaskDetail() {
+    alert("showTaskDetail is being called")
+    // $("#task-detail").show();
+    // $("#task-detail h2").text(newTask.taskName);
+    // $(".detail-duedate").text(newTask.dueDate);
+    // $(".detail-urgency").text(newTask.urgencyLevel);
+    // $(".detail-category").text(newTask.categoryType);
+    // $(".detail-notes").text(newTask.notesInfo);
+    }
+
 // user interface logic
 $(document).ready(function() {
   $("form#todo-form").submit(function() {
-  event.preventDefault();
+    event.preventDefault();
 
-  var inputtedTask = $("#task-name").val();
-  var inputtedDate = $("#due-date").val();
-  var inputtedUrgency = $("#urgency-level").val();
-  var inputtedCategory = $("input:radio[name=category-type]:checked").val();
-  var inputtedNotes = $("#notes-box").val();
+    var inputtedTask = $("#task-name").val();
+    var inputtedDate = $("#due-date").val();
+    var inputtedUrgency = $("#urgency-level").val();
+    var inputtedCategory = $("input:radio[name=category-type]:checked").val();
+    var inputtedNotes = $("#notes-box").val();
 
-  var newTask = new Task(inputtedTask, inputtedDate, inputtedUrgency, inputtedCategory, inputtedNotes);
-  var taskId = inputtedTask.concat();
-  $("form#unfinished").append("<div class='form-check' id='" + taskId + "'>" + "<input id='list-item' class='form-check-input' type='checkbox' value='' id='defaultCheck1'>" + "<label class='form-check-label clickable' for='defaultCheck1'>" + inputtedTask + "</label>" + "</div>");
+    var newTask = new Task(inputtedTask, inputtedDate, inputtedUrgency, inputtedCategory, inputtedNotes);
+    var taskId = inputtedTask.concat();
+    $("form#unfinished").append("<div class='form-check' id='" + taskId + "'>" + "<input id='" + taskId + "toRemove' class='form-check-input' type='checkbox' value='' id='defaultCheck1'>" + "<label class='form-check-label clickable' for='defaultCheck1'>" + newTask.taskName + "</label>" + "</div>");
 
-  $(".form-check-label").click(function() {
-    $("#task-detail").show();
-    $("#task-detail h2").text(newTask.taskName);
-    $(".detail-duedate").text(newTask.dueDate);
-    $(".detail-urgency").text(newTask.urgencyLevel);
-    $(".detail-category").text(newTask.categoryType);
-    $(".detail-notes").text(newTask.notesInfo);
+    $(".form-check-label").last().click(function() {
+      $("#task-detail").show();
+      $("#task-detail h2").text(newTask.taskName);
+      $(".detail-duedate").text(newTask.dueDate);
+      $(".detail-urgency").text(newTask.urgencyLevel);
+      $(".detail-category").text(newTask.categoryType);
+      $(".detail-notes").text(newTask.notesInfo);
     });
 
-    $("#list-item").click(function() {
+    $("#" + taskId + "toRemove").click(function() {
       $("#" + taskId).remove();
-      $("form#complete").append("<div class='form-check'>" + "<input id='list-item' class='form-check-input' type='checkbox' value='' id='defaultCheck1' checked>" + "<label class='form-check-label clickable' for='defaultCheck1'>" + inputtedTask + "</label>" + "</div>");
+      $("form#complete").append("<div class='form-check' id='" + taskId + "' >" + "<input id='" + taskId + "toRemove' class='form-check-input' type='checkbox' value='' id='defaultCheck1' checked>" + "<label class='form-check-label' for='defaultCheck1'>" + newTask.taskName + "</label>" + "</div>");
+
+      $("#complete input").last().click(function(){
+        $("#" + taskId).remove();
+        $("form#unfinished").append("<div class='form-check' id='" + taskId + "'>" + "<input id='" + taskId + "toRemove' class='form-check-input' type='checkbox' value='' id='defaultCheck1'>" + "<label class='form-check-label clickable' for='defaultCheck1'>" + newTask.taskName + "</label>" + "</div>");
+
+        $("#" + taskId + "toRemove").click(function() {
+          $("#" + taskId).remove();
+          $("form#complete").append("<div class='form-check' id='" + taskId + "' >" + "<input id='" + taskId + "toRemove' class='form-check-input' type='checkbox' value='' id='defaultCheck1' checked>" + "<label class='form-check-label' for='defaultCheck1'>" + newTask.taskName + "</label>" + "</div>");
+        })
+        // document.getElementByClassName("form-check-input").addEventListener("click", showTaskDetail);
 
       });
+    });
 
+  console.log("end of function");
   });
 });
